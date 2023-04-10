0c8345481e5580c2fbb6b9001c5b13ff0af26c22
Alvaro <alvaro.guillenc1612@gmail.com>
2023-04-10 17:08:53 -0500

    connection to supabase done

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { app } from "@azure/functions";
 import { test } from "./functions/test";
+import { testConnection } from "./functions/testConnection";
 
 app.http('test', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: test
+});
+app.http('testConnection', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: testConnection
 });
